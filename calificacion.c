4831357de0e47b4da59cb8aee904805735e2927c
urielrs <162546336+urielrs@users.noreply.github.com>
2024-09-22 22:26:14 -0600

    Add files via upload

---
+++ calificacion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+  int main(){
+      int cal;
+      puts("Introduce una calificación");
+      scanf("%d", &cal);
+      if (cal >= 70){
+      printf("Muchas Felicidades");
+      }
+      printf("Fin del programa");
+      return 0;
+  }
