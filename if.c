20e0d9023b30103ddb2a6ef163c41c8bcd6cb30a
urielrs <162546336+urielrs@users.noreply.github.com>
2024-10-05 23:00:58 -0600

    Add files via upload

---
+++ if.c
@@ -0,0 +1,12 @@
+    #include <stdio.h>
+    #include <stdbool.h>
+    
+    int main (){
+    if(true){
+      printf("Todo este ");
+      printf("código, se imprime dentro ");
+      printf("del bloque if ");
+      }
+      return 0;
+    }
+    
