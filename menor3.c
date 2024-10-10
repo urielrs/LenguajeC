ff2b7e1f0c3ed2e47d95a923d685d50add032847
urielrs <162546336+urielrs@users.noreply.github.com>
2024-10-09 23:49:49 -0600

    Add files via upload

---
+++ menor3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (){
+  int numero = 1;
+  
+  while(numero>0 && numero<=1000){
+  if(numero % 3 == 0){
+  
+  
+  printf ("%d,\n ", numero);
+  
+  }
+  numero++;
+  }
+  
+return 0;
+
+}
+
+
+
+
